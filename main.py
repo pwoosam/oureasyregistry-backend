b96bd20e9b22596de757598439eafbfe7b13f169
Patrick Woo-Sam <pwoosam5@icloud.com>
2018-04-21 20:31:29 -0700

    Added swagger

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restplus import Api, Resource, fields
 from utilities import send_text
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Our Easy Registry API')
 
-@app.route('/')
-@app.route('/index')
-def welcome_page():
-    return ""
+sms_ns = api.namespace('sms', description='Send and recieve SMS texts')
+phone_message_model = api.model('Phone Message', {
+    'phone_number': fields.Integer(required=True, example=17143335555, description="A phone number including country code"),
+    'message': fields.String(required=True, description="The message to send to the number")
+})
 
 
-@app.route('/send_text/<int:phone_number>/<message>')
-def send_message_to_number(phone_number: int, message: str):
-    send_text(phone_number, message)
-    return f"Sent {message} to {phone_number}"
+@sms_ns.route('/')
+class SMS(Resource):
+    '''Enables two factor authentication.'''
+    @sms_ns.expect(phone_message_model)
+    def post(self):
+        '''Send a text message'''
+        body = request.get_json()
+        phone_number = body.get('phone_number')
+        message = body.get('message')
+        response = send_text(phone_number, message)
+        if response.status_code == 200:
+            return f"Sent {message} to {phone_number}"
+        else:
+            return f"Failed to send {message} to {phone_number}: {response.body}"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="9001")
+    app.run(host="0.0.0.0", port=9001)
