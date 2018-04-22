d5f3e47bca29b18705429ac9d17aafbc608da9ec
Patrick Woo-Sam <pwoosam5@icloud.com>
2018-04-22 14:48:28 -0700

    Added peewee for the db and the ability to create, add, remove and modify items

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restplus import Api, Resource, fields
 from flask_cors import CORS
 from utilities import send_text
-from in_memory_db import create_table, insert_row, get_row, delete_row
+from peewee import SqliteDatabase, Model, CharField, BooleanField, DecimalField
 
 # setup app and CORS
 app = Flask(__name__)
 CORS(app)
 api = Api(app, version='1.0', title='Our Easy Registry API')
+
+db = SqliteDatabase('ezregistry.db')
+
+class Item(Model):
+    name = CharField()
+    price = DecimalField()
+    url = CharField()
+    is_purchased = DecimalField()
+
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([Item])
 
 sms_ns = api.namespace('sms', description='Send and recieve SMS texts')
 phone_message_model = api.model('Phone Message Model', {
@@ -33,53 +47,80 @@ class SMS(Resource):
 
 
 db_ns = api.namespace('db', description='Database commands')
-create_table_model = api.model('Create Table Model', {
-    'table_name': fields.String(required=True),
-    'fields': fields.List(fields.String(), required=True)
+create_item_model = api.model('Create Item Model', {
+    'name': fields.String(required=True),
+    'price': fields.Float(required=True),
+    'url': fields.String(required=True)
 })
-get_row_model = api.model('Get Row Model', {
-    'table_name': fields.String(required=True),
-    'column': fields.String(required=True),
-    'value': fields.String(required=True)
+update_item_model = api.model('Insert Row Model', {
+    'name': fields.String(required=True),
+    'is_purchased': fields.Boolean(required=True)
 })
-insert_row_model = api.model('Insert Row Model', {
-    'table_name': fields.String(required=True),
-    'columns': fields.List(fields.String(), required=True),
-    'values': fields.List(fields.String(), required=True)
+delete_item_model = api.model('Delete Item Model', {
+    'name': fields.String(required=True)
+})
+item_model = api.model('Item Model', {
+    'name': fields.String(required=True),
+    'price': fields.Float(required=True),
+    'url': fields.String(required=True),
+    'is_purchased': fields.Boolean(required=True)
 })
 
 
-@db_ns.route('/')
+@db_ns.route('/item')
 class DB(Resource):
-    @db_ns.expect(create_table_model)
+    @db_ns.marshal_list_with(item_model)
+    def get(self):
+        '''Get all items'''
+        items = Item.select()
+        x = []
+        for item in items:
+            x.append({
+                'name': str(item.name),
+                'price': float(item.price),
+                'url': str(item.url),
+                'is_purchased': bool(item.is_purchased)
+            })
+
+        return x
+
+    @db_ns.expect(update_item_model)
     def put(self):
-        '''Create a table'''
+        '''Update an existing item'''
         body = request.get_json()
-        table_name = body.get('table_name')
-        fields = body.get('fields')
-        create_table(table_name, fields)
+        name = body.get('name')
+        is_purchased = body.get('is_purchased')
+        item = Item.select().where(Item.name == name)
+        item.is_purchased = is_purchased
+        item.save()
 
-    @db_ns.expect(insert_row_model)
+    @db_ns.expect(create_item_model)
     def post(self):
-        '''Insert a row into a table'''
+        '''Create a new item'''
         body = request.get_json()
-        table_name = body.get('table_name')
-        columns = body.get('columns')
-        values = body.get('values')
+        name = body.get('name')
+        price = body.get('price')
+        url = body.get('url')
         
-        data = {}
-        for key, val in zip(columns, values):
-            data[key] = val
-
-        insert_row(table_name, **data)
+        item = Item.create(name=name, price=price, url=url, is_purchased=False)
+        item.save()
 
 
-@db_ns.route('/<table_name>/<column>/<value>')
+@db_ns.route('/<name>')
 class DBGet(Resource):
-    def get(self, table_name, column, value):
-        '''Gets a row from a table by the value of a column'''
-        data = {column: value}
-        return get_row(table_name, **data)
+    @db_ns.marshal_with(item_model, envelope='resource')
+    def get(self, name):
+        '''Gets a single item by name'''
+        try:
+            item = Item.select().where(Item.name == name).get()
+        except Exception:
+            return abort(404)
+        return {
+            'name': str(item.name),
+            'price': float(item.price),
+            'url': str(item.url),
+            'is_purchased': bool(item.is_purchased)
+        }
 
 
 if __name__ == '__main__':
